90ce8d1bef9d43e617d2e324a446d006b946a6f8
Kristian Freeman <kristian@kristianfreeman.com>
2024-10-25 12:43:52 -0500

    2024-10-25-12:43:52

--- scripts/insert_site.sql
+++
@@ -1,7 +0,0 @@
--- Insert a site
-
--- INSERT INTO sites (name, sitemap_url)
-  -- VALUES ('Example Site', 'http://example.com/sitemap.xml');
-
-INSERT INTO sites (name, sitemap_url)
-  VALUES ('kristianfreeman.com', 'https://kristianfreeman.com/sitemap-index.xml');
---
+++ scripts/insert_sites.example.sql
@@ -0,0 +1,5 @@
+-- Insert a site
+-- Execute using npx wrangler d1 execute index_db --file scripts/insert_sites.sql
+
+-- INSERT INTO sites (name, sitemap_url)
+  -- VALUES ('Example Site', 'http://example.com/sitemap.xml');
